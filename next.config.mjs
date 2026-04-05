50e129e15c7389e0ff5d9a8bde4c3f41a54f5261
cslcs-gen <cslcs.gen@gmail.com>
2026-04-05 20:34:18 +0800

    fix: remove @opennextjs/cloudflare completely and reset next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
 export default nextConfig;
-
-import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
